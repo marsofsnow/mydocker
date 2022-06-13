712cc780a361200dd2ec3bd60be1054be9772903
zh <marsofsnow@outlook.com>
2022-06-13 18:44:30 +0800

    add code

---
+++ start.sh
@@ -0,0 +1,3 @@
+docker-compose up   elasticsearch kibana go-stash filebeat  zookeeper kafka  prometheus grafana  jaeger
+#docker-compose down    prometheus grafana  jaeger
+#docker-compose up -d prometheus grafana  jaeger
